c15a25a5ecafc517a32c399cf1405bab0368cc7f
Diego Couto <die_go0107@yahoo.com.br>
2022-09-15 23:08:18 -0300

    Adicionando specification de biblioteca externa no authuser

--- authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -4,11 +4,12 @@ import com.ead.authuser.models.UserModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
 public class SpecificationTemplate {
-    @And({
+    @Or({
         @Spec(path = "userType", spec = Equal.class),
         @Spec(path = "userStatus", spec = Equal.class),
         @Spec(path = "email", spec = Like.class)
