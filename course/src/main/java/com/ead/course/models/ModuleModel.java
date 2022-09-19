733438cc55aa2ebed2bde2a0926fb5982b16fec4
Diego Couto <die_go0107@yahoo.com.br>
2022-09-19 11:39:30 -0300

    criando estrutura e mapeamento das entidades de course

---
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -0,0 +1,45 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)//não serializa atributos com campos nulos
+@Entity
+@Table(name = "TB_MODULES")
+public class ModuleModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID moduleId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//Não mostra no getAllModulleModel, só se for alterar o campo
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "FK_course")
+    private CourseModel course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "module", fetch = FetchType.LAZY)
+    private Set<LessonModel> lessons;
+
+
+}
