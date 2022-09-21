25f580eb4c621b738cc3b2eaf57054bb705ded44
Diego Couto <die_go0107@yahoo.com.br>
2022-09-21 16:01:19 -0300

    adicionando endpoints do MODULE

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,12 +2,21 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
     List<ModuleModel> findAllModuleModelByCourse_courseId(UUID courseId);
+
+    @Query(value = "SELECT module " +
+            " from ModuleModel module " +
+            " JOIN module.course course " +
+            " where module.moduleId = :moduleId " +
+            " and course.courseId = :courseId ")
+    Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId);
 
     /**
      * Nesse exemplo, utilizando o @EntityGraph ele irá despresar o FetchType.LAZY do atributo course e irá trazer como
